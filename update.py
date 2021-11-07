64043a49e43c4e879264f5fced0bef0f5552594c
David-Valters <46930139+David-Valters@users.noreply.github.com>
2021-11-07 18:28:36 +0200

    Update update.py

--- update.py
+++ update.py
@@ -4,7 +4,7 @@ import sys
 
 import libery
 
-ver='v1.0.0'
+ver='v1.0.1'
 url_ver='https://api.github.com/repos/David-Valters/Animevost_CLI/releases'
 url_file_list='https://api.github.com/repos/David-Valters/Animevost_CLI/git/trees/main?recursive=1'
 
